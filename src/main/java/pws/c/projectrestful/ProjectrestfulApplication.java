195959cedc3f1fd954e89cb10a0770464c257f86
niaaluf <niaaudita01@gmail.com>
2022-12-04 16:50:45 +0700

    membuat java class productservicecontroller

---
+++ src/main/java/pws/c/projectrestful/ProjectrestfulApplication.java
@@ -0,0 +1,13 @@
+package pws.c.projectrestful;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectrestfulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectrestfulApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pws/c/projectrestful/ProjectrestfulApplicationTests.java
@@ -0,0 +1,13 @@
+package pws.c.projectrestful;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectrestfulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
