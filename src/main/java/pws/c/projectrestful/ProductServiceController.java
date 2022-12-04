c27a93479305832c6e1c3787c8193e9055b8a7b9
niaaluf <niaaudita01@gmail.com>
2022-12-04 17:24:04 +0700

    menambahkan delete pada controller

--- src/main/java/pws/c/projectrestful/ProductServiceController.java
+++ src/main/java/pws/c/projectrestful/ProductServiceController.java
@@ -5,10 +5,39 @@
  */
 package pws.c.projectrestful;
 
+import java.util.HashMap;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  *
  * @author asus
  */
+@RestController
 public class ProductServiceController {
+    private static Map<String, Product> productRepo = new HashMap<>();
+    static {
+        Product buah = new Product();
+        buah.setId("1");
+        buah.setName("Apel");
+        productRepo.put(buah.getId(), buah);
+        
+        Product hewan = new Product();
+        hewan.setId("2");
+        hewan.setName("Naga");
+        productRepo.put(hewan.getId(), hewan);
+    }
+    
+   @RequestMapping(value = "/products/{id}", method = RequestMethod.DELETE)
+    public  ResponseEntity<Object> delete(@PathVariable("id") String id){
+        productRepo.remove(id);
+        return new ResponseEntity<>("Product is deleted successfully", HttpStatus.OK);
+    }
+    
+    
     
 }
